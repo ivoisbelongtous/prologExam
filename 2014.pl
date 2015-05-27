7f72e65fb4f5dfe7fe8e024dc4007694deb0d65e
Adam Davies <adamdavies1915@gmail.com>
2015-05-27 13:38:53 +0100

    change  indentation to spaces

--- 2014.pl
+++ 2014.pl
@@ -1,20 +1,20 @@
 a(_,0).
-a(N,A) :- 	N > 0,
-			M is N - 1,
-			a(M,B),
-			A is B + 1.
+a(N,A) :-   N > 0,
+            M is N - 1,
+            a(M,B),
+            A is B + 1.
 
 b(0).
 b(1).
 b(N) :- a(N,A),
-		A > 1,
-		A * A =< N,
-		0 =:= N mod A.
+        A > 1,
+        A * A =< N,
+        0 =:= N mod A.
 c(1).
 
 d(N) :- b(N),
-		!,
-		c(0).
+        !,
+        c(0).
 
 d(_).
 
