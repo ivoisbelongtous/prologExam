124d4f8f206f8e9a3cbdd900f475d316dcfc7367
Adam Davies <adamdavies1915@gmail.com>
2015-05-27 14:51:55 +0100

    added comments

--- 2013Q3.pl
+++ 2013Q3.pl
@@ -1,17 +1,25 @@
 s(L) :- sS(L,[]).
 
 sS([a|L],M) 	:- sT(L,M).
+%% S --> aT
 
 sT([a|L],M) 	:- sX(L,M).
+%% T --> aX
 st([b,a|L],N) 	:- sT(L,M),
 				   sV(M,N).
+%% T --> baTV
 
 sX([],[]).
-sX([a|L],[a|L]).
-sX([c|L],[c|L]).
-sX([a|L],[a|L]).
+sX([a|L],[a|L]). %
+sX([c|L],[c|L]). % not sure what these do
+sX([a|L],[a|L]). %
+%% X --> e
+
 sX([b|L],N) 	:- sT(L,M),
 				   sV(M,N).
+%% X --> bTV
 
 sV([a,c|L],L).
+%% V --> ac
 sV([c,a|L],L).
+%% V --> ca
